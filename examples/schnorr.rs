194902a861ec1a30d9c310d6b5ef6f0b7b302b8c
Raphael Toledo <raphael.toledo@iohk.io>
2026-02-06 19:11:02 +0000

    Cleaning up examples

--- examples/schnorr.rs
+++ examples/schnorr.rs
@@ -1,41 +1,42 @@
 use anyhow::{Context as _, Result, anyhow};
 use group::Group;
-use midnight_circuits::instructions::hash::HashCPU;
-use midnight_curves::{Base, Bls12, BlsScalar as Scalar, Fq, G1Projective};
-use midnight_curves::{Fr as JubjubScalar, JubjubAffine, JubjubExtended as Jubjub, JubjubSubgroup};
-
-use midnight_proofs::{
-    plonk::{create_proof, k_from_circuit, keygen_pk, keygen_vk, prepare},
-    poly::{
-        commitment::{Guard, PolynomialCommitmentScheme},
-        kzg::{
-            KZGCommitmentScheme,
-            params::{ParamsKZG, ParamsVerifierKZG},
-        },
-    },
-    transcript::Transcript,
-};
+use log::{debug, info};
+use rand::rngs::StdRng;
+use rand_core::SeedableRng;
+use std::fs::File;
 
 use midnight_circuits::hash::poseidon::PoseidonChip;
-use midnight_proofs::circuit::Value;
+use midnight_circuits::instructions::hash::HashCPU;
+use midnight_curves::{Base, Bls12, BlsScalar as Scalar, Fq};
+use midnight_curves::{Fr as JubjubScalar, JubjubAffine, JubjubExtended as Jubjub, JubjubSubgroup};
+use midnight_proofs::{
+    circuit::Value,
+    plonk::{
+        ProvingKey, VerifyingKey, create_proof, k_from_circuit, keygen_pk, keygen_vk, prepare,
+    },
+    poly::commitment::Guard,
+    poly::kzg::{
+        KZGCommitmentScheme,
+        params::{ParamsKZG, ParamsVerifierKZG},
+    },
+    transcript::{CircuitTranscript, Transcript},
+};
 use midnight_zk_stdlib::MidnightCircuit;
+use midnight_zk_stdlib::Relation;
 
-use cardhalo::{
+use plutus_halo2_verifier_gen::plutus_gen::{
+    CardanoFriendlyBlake2b, export_proof, export_public_inputs, generate_aiken_verifier,
+    generate_plinth_verifier, serialize_proof,
+};
+use plutus_halo2_verifier_gen::{
     circuits::schnorr_circuit::{SchnorrExample, SchnorrSignature, utils::verify},
     kzg_params::get_or_create_kzg_params,
 };
-use log::{debug, info};
-use midnight_zk_stdlib::Relation;
-use rand::rngs::StdRng;
-use rand_core::SeedableRng;
 
-#[path = "./utils.rs"]
-mod utils;
-use utils::{CTranscript, PCS, PK, Params, VK, export_all};
-
-fn main() -> Result<()> {
-    compile_schnorr_circuit::<KZGCommitmentScheme<Bls12>>()
-}
+pub type KZG = KZGCommitmentScheme<Bls12>;
+pub type Params = ParamsKZG<Bls12>;
+pub type ParamsVK = ParamsVerifierKZG<Bls12>;
+pub type CTranscript = CircuitTranscript<CardanoFriendlyBlake2b>;
 
 // Returns the affine coordinates of a given Jubjub point.
 fn get_coords(point: &JubjubSubgroup) -> (Base, Base) {
@@ -44,14 +45,9 @@ fn get_coords(point: &JubjubSubgroup) -> (Base, Base) {
     (point.get_u(), point.get_v())
 }
 
-fn compile_schnorr_circuit<
-    S: PolynomialCommitmentScheme<
-            Scalar,
-            Commitment = G1Projective,
-            Parameters = ParamsKZG<Bls12>,
-            VerifierParameters = ParamsVerifierKZG<Bls12>,
-        >,
->() -> Result<()> {
+fn main() -> Result<()> {
+    env_logger::init();
+
     // Signing and public keys
     let shnorr_sk = JubjubScalar::from(7);
     let schnorr_pk = JubjubSubgroup::generator() * shnorr_sk;
@@ -76,7 +72,7 @@ fn compile_schnorr_circuit<
             let e = JubjubScalar::from_bytes_wide(&buff);
             k - e * shnorr_sk
         };
-
+ 
         SchnorrSignature { s, e_bytes }
     };
 
@@ -103,8 +99,10 @@ fn compile_schnorr_circuit<
     );
     let k = k_from_circuit(&circuit);
     let params: Params = get_or_create_kzg_params(k, rng.clone())?;
-    let vk: VK = keygen_vk(&params, &circuit).context("keygen_vk should not fail")?;
-    let pk: PK = keygen_pk(vk.clone(), &circuit).context("keygen_pk should not fail")?;
+    let vk: VerifyingKey<Scalar, KZG> =
+        keygen_vk(&params, &circuit).context("keygen_vk should not fail")?;
+    let pk: ProvingKey<Scalar, KZG> =
+        keygen_pk(vk.clone(), &circuit).context("keygen_pk should not fail")?;
 
     let mut transcript = CTranscript::init();
     debug!("transcript: {:?}", transcript);
@@ -127,11 +125,7 @@ fn compile_schnorr_circuit<
     let proof = transcript.finalize();
 
     let mut invalid_proof = proof.clone();
-    // index points to bytes of first scalar that is part of the proof
-    // this should be safe and not result in malformed encoding exception
-    // which is likely for flipping Byte for compressed G1 element
-    // simple mul has 8 G1 elements at the beginning of the proof each 48 bytes long
-    let index = 48 * 8 + 2;
+    let index = 48 * 22 + 2;
     let firs_byte = invalid_proof[index];
     let negated_firs_byte = !firs_byte;
     invalid_proof[index] = negated_firs_byte;
@@ -139,7 +133,7 @@ fn compile_schnorr_circuit<
     info!("proof size {:?}", proof.len());
 
     let mut transcript_verifier = CTranscript::init_from_bytes(&proof);
-    let verifier = prepare::<_, PCS, CTranscript>(&vk, &[&[]], instances, &mut transcript_verifier)
+    let verifier = prepare::<_, KZG, CTranscript>(&vk, &[&[]], instances, &mut transcript_verifier)
         .context("prepare verification failed")?;
 
     verifier
@@ -147,5 +141,42 @@ fn compile_schnorr_circuit<
         .map_err(|e| anyhow!("{e:?}"))
         .context("verify failed")?;
 
-    export_all(proof, params, vk, instances, invalid_proof)
+    let instances_file =
+        "./plinth-verifier/plutus-halo2/test/Generic/serialized_public_input.hex".to_string();
+    let mut output = File::create(instances_file).context("failed to create instances file")?;
+    export_public_inputs(instances, &mut output).context("failed to export public inputs")?;
+
+    serialize_proof(
+        "./plinth-verifier/plutus-halo2/test/Generic/serialized_proof.json".to_string(),
+        proof.clone(),
+    )
+    .context("json proof serialization failed")?;
+
+    export_proof(
+        "./plinth-verifier/plutus-halo2/test/Generic/serialized_proof.hex".to_string(),
+        proof.clone(),
+    )
+    .context("hex proof serialization failed")?;
+
+    generate_plinth_verifier(&params, &vk, instances)
+        .context("Plinth verifier generation failed")?;
+
+    generate_aiken_verifier(
+        &params,
+        &vk,
+        instances,
+        Some((proof.clone(), invalid_proof)),
+    )
+    .context("Aiken verifier generation failed")?;
+    export_proof(
+        "./aiken-verifier/submitter/serialized_proof.hex".to_string(),
+        proof,
+    )
+    .context("hex proof serialization failed")?;
+
+    let instances_file = "./aiken-verifier/submitter/serialized_public_input.hex".to_string();
+    let mut output = File::create(instances_file).context("failed to create instances file")?;
+    export_public_inputs(instances, &mut output).context("Failed to export the public inputs")?;
+
+    Ok(())
 }
